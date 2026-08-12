42f9e2e78e41512e60eb92ad76626d2a8dbe7e34
pranesh1703 <praneshprajesh46@gmail.com>
2026-08-12 13:52:26 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,22 @@
+a=int(input("Enter the length of the rectangle: "))
+b=int(input("Enter the width of the rectangle: "))
+area=a*b
+print(area)
+
+a=int(input("Enter the length of the rectangle: "))
+b=int(input("Enter the width of the rectangle: "))
+area=a*b
+print("The area of the rectagle is: ",area)
+
+
+a=int(input("Enter the length of the rectangle: "))
+b=int(input("Enter the width of the rectangle: "))
+area=a*b
+print(type(area))
+print("The area of rectangle is %d"%(area))
+
+a=int(input("Enter the length of the rectangle: "))
+b=int(input("Enter the width of the rectangle: "))
+peri=2*(a*b)
+print(type(peri))
+print("The perimeter of rectangle is %d"%(peri))
