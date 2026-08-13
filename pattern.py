fcc24bdc435d785fa9aafcd6e7710d732d7e23c6
pranesh1703 <Praneshprajesh46@gmail.com>
2026-08-13 13:25:04 +0530

    Initial commit

---
+++ pattern.py
@@ -0,0 +1,42 @@
+#For star pattern
+a=int(input("Enter number rows: "))
+for i in range(1,a+1):
+    for j in range(1,i+1):
+        print("*", end= " ")
+    print()
+
+
+
+#For number
+a=int(input("Enter number rows: "))
+for i in range(1,a+1):
+    for j in range(1,i+1):
+        print(i, end= " ")
+    print()
+
+
+
+#For number
+a=int(input("Enter number rows: "))
+for i in range(1,a+1):
+    for j in range(1,i+1):
+        print(j, end= " ")
+    print()
+
+
+#For number
+a=int(input("Enter number rows: "))
+for i in range(1,a+1):
+    for j in range(1,i+1):
+        print(a, end= " ")
+    print()
+
+
+#For number
+a=int(input("Enter number rows: "))
+num=1
+for i in range(1,a+1):
+    for j in range(1,i+1):
+        print(num, end= " ")
+        num=num+1
+    print()    
