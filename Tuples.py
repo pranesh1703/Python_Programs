aba42c32d23beb5f850e4fe1bfffbdee016be285
pranesh1703 <Praneshprajesh46@gmail.com>
2026-08-18 10:12:06 +0530

    Initial commit

---
+++ Tuples.py
@@ -0,0 +1,15 @@
+tup1=(1,2,3,"Hello","H")
+print(tup1)
+
+
+avengers=("Thor","Hulk","Ironman","Spiderman","CaptainAmerica")
+print(avengers)
+print(avengers[2])
+print(avengers[-2])
+print(avengers[0:2])
+print(avengers[-3:-1])
+print(len(avengers))
+print(min(avengers))
+str1="Pranesh";
+a=tuple(str1)
+print(a)
