8545e746841ec08f2594626f8504b305c7a7ec7b
pranesh1703 <Praneshprajesh46@gmail.com>
2026-08-20 11:27:11 +0530

    Initial commit

---
+++ Management_sys.py
@@ -0,0 +1,17 @@
+name="aRun KumAR"
+email="ARUN.KUMAR@GMAIL.COM"
+id="emp-2026-015"
+deparment="software development"
+number="987654321"
+skills="Python,java,sql,python"
+code="school of inovation"
+
+
+print("\n---------------------------------\nPython Developer Managment System\n---------------------------------")
+print("Employee's name:",name.title())
+print("Email:",email.lower())
+print("Employee's ID:",id.upper())
+print("Department:",deparment.title())
+print("Mobile number:",number)
+print("Skills:",skills.replace("python","C++"))
+print("Company code:",code.upper())
